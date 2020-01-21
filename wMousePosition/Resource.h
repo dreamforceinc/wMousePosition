78bd992fbef461ecdd3205092a8a24dd8a9b62f4
dreamforceinc <dreamforce@yandex.ru>
2020-01-21 16:28:21 +0300

    Added version resource

--- wMousePosition/Resource.h
+++ wMousePosition/Resource.h
@@ -1,25 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
 // Used by wMousePosition.rc
-
-#define IDS_APP_TITLE		100
-#define IDC_WMOUSEPOSITION	101
-#define IDI_WMOUSEPOSITION	102
-#define IDS_RUNNING			103
-#define IDS_ERR_MAIN		104
-#define IDS_ERR_WND			105
-#define IDS_ERR_HOOK		106
-
-#ifndef IDC_STATIC
-#define IDC_STATIC			-1
-#endif
-// Next default values for new objects
 //
+#define IDS_APP_TITLE                   100
+#define IDC_WMOUSEPOSITION              101
+#define IDI_WMOUSEPOSITION              102
+#define IDS_RUNNING                     103
+#define IDS_ERR_MAIN                    104
+#define IDS_ERR_WND                     105
+#define IDS_ERR_HOOK                    106
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
